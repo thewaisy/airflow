7eae1af71dd800529d26913d2a565976134754fe
hanyoon <thewaisy@gmail.com>
2022-02-04 16:30:47 +0900

    airflow 2.2.3

--- build_image_and_push.sh
+++ build_image_and_push.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-echo "build docker image"
-echo "Airflow version: ${1}"
-echo "Python version: ${2}"
-
+AIRFLOW_VERSION='2.2.3'
+PYTHON_VERSION='3.8'
 USER='waisy'
+
+echo "build docker image"
+echo "Airflow version: ${AIRFLOW_VERSION}"
+echo "Python version: ${PYTHON_VERSION}"
+
 
 # build image
 docker build \
-    -t $USER/airflow:$1-$2 \
-    --build-arg AIRFLOW_VERSION=$1 \
+    -t $USER/airflow:$AIRFLOW_VERSION-$PYTHON_VERSION \
+    --build-arg AIRFLOW_IMAGE_NAME="apache/airflow:${AIRFLOW_VERSION}-python${PYTHON_VERSION}" \
     ./docker
 
 # docker image push
